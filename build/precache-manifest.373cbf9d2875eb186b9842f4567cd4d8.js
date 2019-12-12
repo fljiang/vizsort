131ad439ad75b656a901dfb949e652c0ee1be233
fljiang <fljiang@edu.uwaterloo.ca>
2019-12-12 00:16:42 -0500

    add build files

---
+++ build/precache-manifest.373cbf9d2875eb186b9842f4567cd4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed42304c53d98010272b125a7c537b3",
+    "url": "/vizsort/index.html"
+  },
+  {
+    "revision": "63acaad904ec0b38a77a",
+    "url": "/vizsort/static/css/main.861c5726.chunk.css"
+  },
+  {
+    "revision": "b6d2f5dce61f4c2ff933",
+    "url": "/vizsort/static/js/2.b5eb1f16.chunk.js"
+  },
+  {
+    "revision": "63acaad904ec0b38a77a",
+    "url": "/vizsort/static/js/main.3ae1f19b.chunk.js"
+  },
+  {
+    "revision": "649e3a6726b9a35ead8e",
+    "url": "/vizsort/static/js/runtime~main.92ae58ef.js"
+  }
+]);
