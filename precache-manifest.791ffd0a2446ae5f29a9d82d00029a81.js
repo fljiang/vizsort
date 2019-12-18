f9527b5b9501757706f89e9774521c2e91347ddc
fljiang <fljiang@edu.uwaterloo.ca>
2019-12-18 18:12:06 -0500

    Updates

--- precache-manifest.791ffd0a2446ae5f29a9d82d00029a81.js
+++ precache-manifest.791ffd0a2446ae5f29a9d82d00029a81.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa07c2906f0a641e4be13d18ff53b303",
+    "revision": "4ab144f8768d2ad9a97bc9c0ee80c08f",
     "url": "/vizsort/index.html"
   },
   {
-    "revision": "00fc3e1d735abdd19f97",
+    "revision": "88858e04a250b22cbd25",
     "url": "/vizsort/static/css/main.861c5726.chunk.css"
   },
   {
-    "revision": "db80a189bf34bdf92cec",
-    "url": "/vizsort/static/js/2.fdb629e0.chunk.js"
+    "revision": "d28ae375d08b03136381",
+    "url": "/vizsort/static/js/2.84afaabe.chunk.js"
   },
   {
-    "revision": "00fc3e1d735abdd19f97",
-    "url": "/vizsort/static/js/main.9e726441.chunk.js"
+    "revision": "88858e04a250b22cbd25",
+    "url": "/vizsort/static/js/main.258a3ae7.chunk.js"
   },
   {
     "revision": "649e3a6726b9a35ead8e",
