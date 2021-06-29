dc00deb3c1b09e19015870b24af3346b65e5661c
Felicia Jiang <fljiang@edu.uwaterloo.ca>
2021-06-28 22:31:01 -0400

    Updates

--- precache-manifest.d094983ba77f1087290aa092062a6822.js
+++ precache-manifest.d094983ba77f1087290aa092062a6822.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "683ecbce0e782b1f95e92526671c0165",
+    "revision": "048f820a1a0035db6eab6661c3376a0b",
     "url": "/vizsort/index.html"
   },
   {
-    "revision": "70e78b3d0de204c8a89f",
+    "revision": "d5c9c956ecff72d9bcdd",
     "url": "/vizsort/static/css/main.619dfb29.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vizsort/static/js/2.1bf96e56.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "70e78b3d0de204c8a89f",
-    "url": "/vizsort/static/js/main.4d319891.chunk.js"
+    "revision": "d5c9c956ecff72d9bcdd",
+    "url": "/vizsort/static/js/main.9f5ed039.chunk.js"
   },
   {
     "revision": "93da61d677f4ee18c59b",
